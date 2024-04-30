2189bef669126b9be10609a6d4f149c485e6c433
Mickaël Ndinga <84698581+Micky-N@users.noreply.github.com>
2024-05-01 01:53:51 +0200

    deploy

--- deploy.php
+++ deploy.php
@@ -36,49 +36,40 @@ host('development')
 
 // Hooks
 
-desc('Deploy your project');
-task('deploy', [
-    'deploy:prepare',
-    'composer:install',
-    'artisan:storage:link',
-    'artisan:config:clear',
-    'artisan:view:clear',
-    'build:env',
-    'npm:install',
-    'npm:build',
-    'deploy:cleanup',
-    'deploy:unlock',
-    'deploy:success',
-])->desc('Deploy Laravel Project');
+// define the paths to PHP & Composer binaries on the server
+set('bin/php', '/usr/bin/php');
+set('bin/npm', '/usr/bin/npm');
+set('bin/composer', '{{bin/php}} /usr/bin/composer');
+
+// compile our production assets
+task('npm:build', function () {
+    run('cd {{release_path}} && {{bin/npm}} install');
+    run('cd {{release_path}} && {{bin/npm}} run build');
+    run('cd {{release_path}} && {{bin/npm}} install --omit=dev');
+})->desc('Compile npm files locally');
 
 // Task to upload .env file
 task('build:env', function () {
     run('cd {{release_path}} && cp .env.{{environment}} .env');
     writeln('Copied .env.{{environment}} to .env');
-});
-
-// Custom task to run npm install
-task('npm:install', function () {
-    run('cd {{release_path}} && npm install');
-});
-
-task('composer:install', function () {
-    run('cd {{release_path}} && composer install');
-});
-
-// Custom task to run npm run production
-task('npm:build', function () {
-    run('cd {{release_path}} && npm run build');
-});
+})->desc('Create environment file');
 
 task('deploy:cleanup', function () {
     $keep = get('keep_releases', 3) + 1;
     run("cd {{deploy_path}}/releases && rm -rf $(ls -t | tail -n +$keep)");
-});
+})->desc('Cleanup old releases');
 
 task('test', function () {
     run('ls -a');
     writeln('success');
 });
 
+task('artisan:migrate')->disable();
+
+after('deploy:vendors', 'build:env');
+after('deploy:vendors', 'npm:build');
+// automatically unlock when a deploy fails
 after('deploy:failed', 'deploy:unlock');
+// after a deploy, clear our cache and run optimisations
+after('deploy:cleanup', 'artisan:cache:clear');
+after('deploy:cleanup', 'artisan:optimize');
